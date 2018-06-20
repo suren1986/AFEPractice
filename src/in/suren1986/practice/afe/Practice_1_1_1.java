542bfb05b1eb36283d9aa95a184486d617d25e3a
酥仁 <suren1986@gmail.com>
2018-06-20 12:48:04 +0800

    添加 1.1.1 联系

---
+++ src/in/suren1986/practice/afe/Practice_1_1_1.java
@@ -0,0 +1,11 @@
+package in.suren1986.practice.afe;
+
+import algs4.cs.princeton.edu.StdOut;
+
+public class Practice_1_1_1 {
+    public static void main(String args[]) {
+        StdOut.println((0 + 15) / 2);
+        StdOut.println(2.0e-6 * 100000000.1);
+        StdOut.println(true && false || true && true);
+    }
+}
